622910074dc4d6a24da6ad55a3c4ec31b0cd5559
brofield <brofield@2c6077ba-a463-11dd-9fb0-994c145e5ff7>
2008-10-27 20:13:36 +0000

    initial add

---
+++ resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by gegg.rc
+//
+#define IDD_GEGG                        101
+#define IDI_GEGG                        102
+#define IDC_MINS                        1001
+#define IDC_SECS                        1002
+#define IDC_START                       1003
+#define IDC_ALARM_FLASH                 1004
+#define IDC_ALARM_FRONT                 1004
+#define IDC_ALARM_SOUND                 1005
+#define IDC_ALARM_MSGBOX                1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
