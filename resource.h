ad68fddc40b08719e38af270c9280276e21f063f
brofield <brofield@2c6077ba-a463-11dd-9fb0-994c145e5ff7>
2008-10-28 15:15:03 +0000

    add hello and tweak the sounds

--- resource.h
+++ resource.h
@@ -5,6 +5,7 @@
 #define IDD_GEGG                        101
 #define IDI_GEGG                        102
 #define IDR_AHEM                        103
+#define IDR_HELLO                       104
 #define IDC_MINS                        1001
 #define IDC_SECS                        1002
 #define IDC_START                       1003
@@ -16,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
