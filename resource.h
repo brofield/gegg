1c9551d16719e4c238cc7a23c24cd16ee73d5e4f
brofield <brofield@2c6077ba-a463-11dd-9fb0-994c145e5ff7>
2008-10-30 08:11:21 +0000

    add about box with clickable hyperlink

--- resource.h
+++ resource.h
@@ -2,24 +2,27 @@
 // Microsoft Visual C++ generated include file.
 // Used by gegg.rc
 //
+#define IDM_ABOUTBOX                    0x0010
 #define IDD_GEGG                        101
 #define IDI_GEGG                        102
 #define IDR_AHEM                        103
 #define IDR_HELLO                       104
+#define IDD_ABOUT                       105
 #define IDC_MINS                        1001
 #define IDC_SECS                        1002
 #define IDC_START                       1003
 #define IDC_SOUND                       1005
 #define IDC_MSGBOX                      1006
 #define IDC_USETRAY                     1008
+#define IDC_APPLINK                     1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
