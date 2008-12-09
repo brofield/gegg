704964dd7c62f0bf76bc9ba7435427badda93c18
brofield <brofield@2c6077ba-a463-11dd-9fb0-994c145e5ff7>
2008-12-09 12:21:53 +0000

    add help to about box and fix it so that it actually can be shown.

--- resource.h
+++ resource.h
@@ -16,6 +16,7 @@
 #define IDC_APPLINK                     1009
 #define IDC_TYPE                        1011
 #define IDC_DIGITAL                     1012
+#define IDC_HELPTEXT                    1013
 
 // Next default values for new objects
 // 
@@ -23,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
